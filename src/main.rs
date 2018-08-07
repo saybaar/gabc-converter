8428c18cedf78874adeb2496756bb7c810ab51e0
Lydia Simmons <saybaar@gmail.com>
2018-08-06 20:25:51 -0700

    Refactored command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,34 @@
 extern crate clap;
 extern crate gabc_parser;
 extern crate serde_json;
-use gabc_parser::{GabcFile, GABCParser, Rule, parse_file};
 use clap::{App, Arg};
+use gabc_parser::{parse_file, GabcFile};
 use std::fs::File;
 use std::io::{self, Read, Write};
 
 fn main() -> io::Result<()> {
     let matches = App::new("gabc-converter")
-        .version("1.0")
-        .author("Kevin K. <kbknapp@gmail.com>")
-        .about("Does awesome things")
+        .version("0.1")
+        .author("Lydia Simmons <saybaar@gmail.com>")
+        .about("Converts a gabc file to JSON or Lilypond")
         .arg(
-            Arg::with_name("INPUT")
-                .help("Sets the input file to use (blank for STDIN)")
-                .required(false)
+            Arg::with_name("TARGET")
+                .long("target")
+                .help("Conversion target")
+                .required(true)
+                .possible_values(&["lilypond", "json", "debug-print"])
                 .index(1),
         )
         .arg(
-            Arg::with_name("target")
-                .long("target")
-                .help("Conversion target (\"lilypond\", \"json\", or \"debug-print\")")
-                .required(true)
-                .takes_value(true)
-                .possible_values(&["lilypond", "json", "debug-print"]),
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .help("Sets the input file to use (blank for STDIN)")
+                .required(false)
+                .takes_value(true),
         )
         .arg(
-            Arg::with_name("OUTPUT")
+            Arg::with_name("output")
                 .short("o")
                 .long("output")
                 .help("Sets the output file to use (blank for STDOUT)")
@@ -35,22 +37,24 @@ fn main() -> io::Result<()> {
         )
         .get_matches();
 
-    let mut input_source: Box<Read> = match matches.value_of("INPUT") {
+    let mut input_source: Box<Read> = match matches.value_of("input") {
         Some(s) => Box::new(File::open(s).expect("Error opening file")),
         None => Box::new(std::io::stdin()),
     };
 
     let mut text = String::new();
-    input_source.read_to_string(&mut text).expect("Error reading file");
+    input_source
+        .read_to_string(&mut text)
+        .expect("Error reading file");
 
-    let output: String = match matches.value_of("target") {
+    let output: String = match matches.value_of("TARGET") {
         Some("json") => GabcFile::new(&text).as_json(),
         Some("lilypond") => GabcFile::new(&text).as_lilypond(),
         Some("debug-print") => gabc_parser::debug_print(parse_file(&text)),
         _ => panic!("Impossible target"),
     };
 
-    let mut output_source: Box<Write> = match matches.value_of("OUTPUT") {
+    let mut output_source: Box<Write> = match matches.value_of("output") {
         Some(s) => Box::new(File::create(s).expect("Error opening file")),
         None => Box::new(std::io::stdout()),
     };
